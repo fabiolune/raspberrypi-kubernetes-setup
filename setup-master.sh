f74f11b1aabe74c9209ca1bcade6eec5d46b38b9
fabiolune <fabiolune@gmail.com>
2021-01-06 22:14:58 +0100

    added readable output

--- setup-master.sh
+++ setup-master.sh
@@ -5,4 +5,8 @@ curl -sfL https://get.k3s.io | sh -s - --no-deploy traefik
 
 # copy generated kube config into default folder
 mkdir -p ~/.kube
-cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+cp /etc/rancher/k3s/k3s.yaml ~/.kube/k3s-config
+echo "To connect to your local cluster you can define an env variable with"
+echo "  export KUBECONFIG=${HOME}/.kube/k3s-config"
+echo "or you can rename your generated config with:"
+echo "  mv ${HOME}/.kube/k3s-config${HOME} ${HOME}/.kube/k3s-config"
