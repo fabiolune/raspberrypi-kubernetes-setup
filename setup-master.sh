4c5570beadeb2e3a21f2e2d2d4ff333fcd704c1a
fabiolune <fabiolune@gmail.com>
2021-01-08 00:09:10 +0100

    fixed ark commands

--- setup-master.sh
+++ setup-master.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-# Install k3s wthout trafik (the default ingress controller)
+# Install arkade
+curl -sLS https://dl.get-arkade.dev | sudo sh
+
+# Install kubectl and helm
+ark get kubectl
+ark get helm
+
+# add ark folder to PATH
+export PATH=$PATH:$HOME/.arkade/bin/
+
+# Install k3s without trafik (the default ingress controller)
 curl -sfL https://get.k3s.io | sh -s - --no-deploy traefik
 
 # copy generated kube config into default folder
 mkdir -p ~/.kube
-cp /etc/rancher/k3s/k3s.yaml ~/.kube/k3s-config
-echo "To connect to your local cluster you can define an env variable with"
-echo "  export KUBECONFIG=${HOME}/.kube/k3s-config"
-echo "or you can rename your generated config with:"
-echo "  mv ${HOME}/.kube/k3s-config${HOME} ${HOME}/.kube/k3s-config"
+sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/k3s-config
+export KUBECONFIG=${HOME}/.kube/k3s-config
